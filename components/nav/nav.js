828550239b96fec010e47a86497d647993ef680b
ArslanPioneer <1041238415@qq.com>
2019-01-17 16:31:08 +0800

    nav组件实现

---
+++ components/nav/nav.js
@@ -0,0 +1,29 @@
+// components/nav/nav.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    title:String,
+    latest:Boolean,
+    first:Boolean
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    title:'',
+    leftSrc:'./images/triangle@left.png',
+    disLeftSrc:'./images/triangle.dis@left.png',
+    rightSrc:'./images/triangle@right.png',
+    disRightSrc:'./images/triangle.dis@right.png'
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
